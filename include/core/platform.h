c884e5b42ade533d50f6802b34a6a9211c5af0bf
Daisuke Sato <sato@kmckk.co.jp>
2023-04-21 19:13:29 +0900

    Add initial support for SOLID

--- include/core/platform.h
+++ include/core/platform.h
@@ -57,6 +57,9 @@ extern "C" {
     #include "platform/lf_zephyr_support.h"
 #elif defined(PLATFORM_NRF52)
     #include "platform/lf_nrf52_support.h"
+#elif defined(PLATFORM_SOLID)
+    // SOLID
+    #include "platform/lf_solid_support.h"
 #elif defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
    // Windows platforms
    #include "lf_windows_support.h"
